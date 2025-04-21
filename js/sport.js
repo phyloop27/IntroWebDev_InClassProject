2ba90970cb670a92cdcadb45306e72cb09652660
Dec <s288984@uos.ac.uk>
2025-04-21 16:09:15 +0100

    "First commit to GitHub 21/04/2025 Intro To WebDev, InClassProject, NewsPage"

---
+++ js/sport.js
@@ -0,0 +1,38 @@
+
+const resultList = document.querySelector('#results');
+const url = 'https://content.guardianapis.com/search?q=sport&api-key=';
+const apiKey = 'e9c90ecd-d8b9-47ab-b0c0-31839b65fb0a';
+fetch(url + apiKey)
+.then((response) => response.json())
+.then((data) => {
+    data.response.results.forEach(function (value) {
+        console.log(value);
+        /*  Code has been commented out here as per Activity 25 -
+            Code relates to labeled comments in sport.js & style.css sheets - Activity 24 and Previous
+                const articleElement = `
+                <li>
+                    <a target="_blank" href="${value.webUrl}">${value.webTitle}</a>
+                </li>`;
+        The code BELOW is for Activity 25 onwards .......    */
+        /*
+                const articleElement = `
+                <div class="col-md-4">
+                    <a target="_blank" href="${value.webUrl}">${value.webTitle}</a>
+                </div>`;
+                resultList.insertAdjacentHTML('beforeend', articleElement);
+            });
+         */
+        const articleElement = `
+            <div class="col-md-4 mb-4"> <!-- Creats the '3 coloum' Layout  -->
+                <div class="card h-100"> <!-- This will Kepp the top's of the cards the same hight  -->
+                    <div class="card-body d-flex flex-column"> <!-- This should keep the article buttum lined up at the bottom  -->
+                        <h5 class="card-title">${value.webTitle}</h5> <!-- Pulls the Value paired with 'card-title' and displays it here -->
+                        <p class="card-text">${value.sectionName}</p> <!-- Pulls the Value paired with 'card-text' and displays it here  -->
+                        <a target="_blank" href="${value.webUrl}" class="btn btn-primary mt-auto">View Article</a> <!-- Opens a new Tab to the Article 'webUrl' value pair  -->
+                    </div>
+                </div>
+            </div>`;
+            resultList.insertAdjacentHTML('beforeend', articleElement);
+        });
+});
+
