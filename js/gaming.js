1bbb9321ba55c2ad1476e8e16340871f8a3aa309
Dec <s288984@uos.ac.uk>
2025-04-22 17:56:24 +0100

    "Change's Include Content on the education & gaming page's - API search completed for each page "

---
+++ js/gaming.js
@@ -0,0 +1,26 @@
+
+const resultList = document.querySelector('#results');
+/*
+Update the URL String - specifically the 'search?q=...' for the required topic
+ */
+const url = 'https://content.guardianapis.com/search?q=gaming&api-key=';
+const apiKey = 'e9c90ecd-d8b9-47ab-b0c0-31839b65fb0a';
+fetch(url + apiKey)
+  .then((response) => response.json())
+  .then((data) => {
+    data.response.results.forEach(function (value) {
+      console.log(value);
+      const articleElement = `
+            <div class="col-md-4 mb-4"> <!-- Creats the '3 coloum' Layout  -->
+                <div class="card h-100"> <!-- This will Kepp the top's of the cards the same hight  -->
+                    <div class="card-body d-flex flex-column"> <!-- This should keep the article buttum lined up at the bottom  -->
+                        <h5 class="card-title">${value.webTitle}</h5> <!-- Pulls the Value paired with 'card-title' and displays it here -->
+                        <p class="card-text">${value.sectionName}</p> <!-- Pulls the Value paired with 'card-text' and displays it here  -->
+                        <a target="_blank" href="${value.webUrl}" class="btn btn-primary mt-auto">View Article</a> <!-- Opens a new Tab to the Article 'webUrl' value pair  -->
+                    </div>
+                </div>
+            </div>`;
+      resultList.insertAdjacentHTML('beforeend', articleElement);
+    });
+  });
+
